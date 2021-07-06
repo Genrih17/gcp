887b4aee7b9e7cd7bbc5dc90f213dca860023b4f
Genrih17 <84070046+Genrih17@users.noreply.github.com>
2021-07-07 00:44:30 +0300

    Update create_TF_instance.tf

--- create_TF_instance.tf
+++ create_TF_instance.tf
@@ -1,35 +1,27 @@
-variable "project_name" {}
-variable "billing_account" {}
-variable "org_id" {}
-variable "region" {}
-
-provider "google" {
-  region = var.region
+data "google_compute_zones" "available" {
+  project = google_project.project.project_id
 }
 
-resource "random_id" "id" {
-  byte_length = 4
-  prefix      = var.project_name
+resource "google_compute_instance" "default" {
+  project      = google_project.project.project_id
+  zone         = data.google_compute_zones.available.names[0]
+  name         = "tf-compute-1"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1604-xenial-v20170328"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  depends_on = [google_project_service.service]
 }
 
-resource "google_project" "project" {
-  name            = var.project_name
-  project_id      = random_id.id.hex
-  billing_account = var.billing_account
-  org_id          = var.org_id
-}
-
-resource "google_project_service" "service" {
-  for_each = toset([
-    "compute.googleapis.com"
-  ])
-
-  service = each.key
-
-  project            = google_project.project.project_id
-  disable_on_destroy = false
-}
-
-output "project_id" {
-  value = google_project.project.project_id
+output "instance_id" {
+  value = google_compute_instance.default.self_link
 }
