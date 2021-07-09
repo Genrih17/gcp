20be96d1c8a742279878a1e2d58e7cfcb724812f
Henadzi Ivashkevich <henadzi_ivashkevich@epam.com>
2021-07-09 02:55:51 -0400

    remembered push task#2

--- module-02/main.tf
+++ module-02/main.tf
@@ -1,6 +1,5 @@
-variable "creation-way"{
+variable "vm_name"{
     type = string
-    default = "terraform"
 }
 
 variable "project"{
@@ -25,19 +24,31 @@ variable "type"{
     type = string
 }
 
+variable "disk_size" {}
+variable "disk_type" {
+  type = string
+}
 variable "labels" {}
-
-variable "system_disk" {}
+variable "vm_timeout_delete" {}
+variable "startup_script"{}
+variable "delete_protection" {}
 
 provider "google" {
+ credentials = file("../CREDENTIALS_FILE.json")
  project     = var.project
  region      = var.region
+ zone        = var.zone
+}
+resource "google_compute_disk" "default" {
+ name = "disk"
+ type = var.disk_type
+ zone = var.zone
+ size = "10"
 }
 
 resource "google_compute_instance" "default" {
- name         = "nginx-${var.creation-way}"
+ name         = var.vm_name 
  machine_type = var.type
- zone         = var.zone
  labels       = var.labels
 
  boot_disk {
@@ -48,17 +59,26 @@ resource "google_compute_instance" "default" {
 
  network_interface {
    network = "default"
- }
+   access_config {}
 }
 
-metadata_startup_script = "yum install -y nginx; systemctl enable nginx; systemctl start nginx"
+ tags = [
+	"http-server",
+	"https-server"
+	]
+ description = "hivashkevich-nginx"
+ deletion_protection = var.delete_protection 
+ timeouts {
+    delete = var.vm_timeout_delete
+  }
+metadata_startup_script = var.startup_script
+}
 
-resource "google_compute_disk" "system_disk" {
-  name  = "system-disk"
-  image = var.image
-  size  = 35
-  type  = "pd-ssd"
-  zone  = "us-central1-c"
+resource "google_compute_attached_disk" "default" {
+ disk = google_compute_disk.default.id
+ instance = google_compute_instance.default.id
+}
+
 output "external_ip" { 
 	value = "http://${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}/"
 } 
--- module-02/var.tfvars
+++ module-02/var.tfvars
@@ -1,10 +1,14 @@
-#instance_name = "nginx-"
+vm_name = "nginx-terraform"
 project = "arcane-shape-319007"
 region = "us-central1"
 zone = "us-central1-c"
 type = "n1-custom-1-4608"
-image = "Cenatos7"
-deletion_protection = "true"
+image = "centos-cloud/centos-7"
+delete_protection = "true"
+disk_type = "pd-ssd"
+disk_size = 35
+startup_script = "yum install -y nginx; systemctl enable nginx; systemctl start nginx"
+vm_timeout_delete = "40m"
 
 labels = {
 	server_type = "nginx_server"
