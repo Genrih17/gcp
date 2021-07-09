d4fcedbc3449701ebe36ef72a159617698ed627c
Henadzi Ivashkevich <henadzi_ivashkevich@epam.com>
2021-07-09 01:26:18 -0400

    module03

---
+++ module-03-vpc/main.tf
@@ -0,0 +1,98 @@
+variable "vm_name"{
+    type = string
+}
+
+variable "project"{
+    type = string
+}
+   
+variable "region"{
+    type = string
+    default = "us-cenral1"
+}
+
+variable "zone"{
+    type = string
+    default = "us-central1-c"
+}
+
+variable "image"{
+    type = string
+}
+
+variable "type"{
+    type = string
+}
+
+variable "disk_size" {}
+variable "disk_type" {
+  type = string
+}
+variable "labels" {}
+variable "vm_timeout_delete" {}
+variable "startup_script"{}
+variable "delete_protection" {}
+variable "student_name" {}
+variable "student_ID"{}
+
+
+
+
+provider "google" {
+ credentials = file("../CREDENTIALS_FILE.json")
+ project     = var.project
+ region      = var.region
+ zone        = var.zone
+}
+
+resource "google_compute_disk" "default" {
+ name = "disk"
+ type = var.disk_type
+ zone = var.zone
+ size = "10"
+}
+
+resource "google_compute_network" "vpc_network" {
+  network = "${var.student_name}-vpc"
+  description = "network with vpc"
+  name = "vpc-network"
+}
+
+
+resource "google_compute_instance" "default" {
+ name         = var.vm_name 
+ machine_type = var.type
+ labels       = var.labels
+
+ boot_disk {
+   initialize_params {
+     image = var.image
+   }
+  }
+
+ network_interface {
+   network = "${var.student_name}-vpc"
+   subnetwork = "subnet"
+   access_config {}
+}
+
+ tags = [
+	"http-server",
+	"https-server"
+	]
+ description = "hivashkevich-nginx-with_vpc"
+ deletion_protection = var.delete_protection 
+ timeouts {
+    delete = var.vm_timeout_delete
+  }
+metadata_startup_script = var.startup_script
+}
+
+resource "google_compute_attached_disk" "default" {
+ disk = google_compute_disk.default.id
+ instance = google_compute_instance.default.id
+}
+
+output "external_ip" { 
+	value = "http://${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}/"
+} 
---
+++ module-03-vpc/var.tfvars
@@ -0,0 +1,19 @@
+vm_name = "nginx-terraform"
+project = "arcane-shape-319007"
+region = "us-central1"
+zone = "us-central1-c"
+type = "n1-custom-1-4608"
+image = "centos-cloud/centos-7"
+delete_protection = "true"
+disk_type = "pd-ssd"
+disk_size = 35
+startup_script = "yum install -y nginx; systemctl enable nginx; systemctl start nginx"
+vm_timeout_delete = "40m"
+student_name = "hivashkevich"
+student_ID = 1
+
+labels = {
+	server_type = "nginx_server"
+	os_family = "redhat"
+	way_of_installation = "terraform"
+}
