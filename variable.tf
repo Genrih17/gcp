31169f43c7fa14b3d4f1764837cb3d5528dc9b5d
Genrih17 <root@localhost.localdomain>
2021-07-20 08:45:20 -0400

    some fixes

---
+++ variable.tf
@@ -0,0 +1,22 @@
+variable "kub_name"{
+    type = string
+}
+
+
+variable "project"{
+    type = string
+}
+
+variable "region"{
+    type = string
+    default = "us-cenral1"
+}
+
+variable "zone"{
+    type = string
+    default = "us-central1-c"
+}
+
+variable "username"{
+    type = string
+}
